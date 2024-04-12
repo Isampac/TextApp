662c2d5bc6544a178b4adc29f8f15f337c024649
Isampac <138738659+Isampac@users.noreply.github.com>
2024-04-12 16:09:25 -0500

    Add files via upload

---
+++ IPTextApp(2).py
@@ -0,0 +1,33 @@
+import streamlit as st
+from langchain import OpenAI
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+
+def generate_response(txt):
+# Instantiate the LLM model
+    llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+    # Split text
+    text_splitter = CharacterTextSplitter()
+    texts = text_splitter.split_text(txt)
+    # Create multiple documents
+    docs = [Document(page_content=t) for t in texts]
+    # Text summarization
+    chain = load_summarize_chain(llm, chain_type='map_reduce')
+    return chain.run(docs)
+
+# Page title
+st.set_page_config(page_title='🦜🦜 Text Summarization App')
+st.title('🦜🦜 Text Summarization App')
+
+# Text input
+txt_input = st.text_area('Enter your text', '', height=200)
+# Form to accept user's text input for summarization
+with st.form('summarize_form', clear_on_submit=True):
+    openai_api_key = st.text_input('OpenAI API Key', type='password')
+    submitted = st.form_submit_button('Submit')
+    if submitted and txt_input and openai_api_key.startswith('sk-'):
+        with st.spinner('Calculating...'):
+            response = generate_response(txt_input, openai_api_key)
+        st.info(response)
+
